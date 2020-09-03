3cde00f99f9a110169379fb27b7151aecc89b5d6
shadman-a <shadman@outlook.com>
2020-09-03 18:34:51 -0400

    git ignored

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyAXvC55IkNhiH6bmvh92OyH9f2Sxftik3k",
+  authDomain: "barberapp-287503.firebaseapp.com",
+  databaseURL: "https://barberapp-287503.firebaseio.com",
+  projectId: "barberapp-287503",
+  storageBucket: '',
+  messagingSenderId: "245663814160",
+  appId: "1:245663814160:web:eff2979c80ff3994707095"
 }
 
 const Firebase = firebase.initializeApp(firebaseConfig)
-
+//git ignored?
 export default Firebase
